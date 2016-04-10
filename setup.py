6b9e8d6d2ef398342804f622f85200cdf1ad2803
ztaira14 <ztaira14@punahou.edu>
2016-04-10 14:14:17 -0400

    PyPI Prep update 4/10
    Started prep for loading this to PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'absolutely_nothing',
+    packages = ['absolutely_nothing'], # this must be the same as the name above
+    version = '0.1',
+    description = 'Does Absolutely Nothing better than anything else',
+    author = 'Zachary Taira',
+    author_email = 'zach.taira@gmail.com',
+    url = 'https://github.com/ztaira14/absolutely_nothing', # use the URL to the github repo
+    download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+    keywords = ['absolutely', 'nothing', 'python'], # arbitrary keywords
+    classifiers = [],
+)
